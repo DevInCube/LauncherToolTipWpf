eac37c3b83bbb1a9edee565c53789f81c3f8f88d
Ruslan Hadyniak <rushad@auditdata.com>
2020-03-17 10:47:00 +0200

    binding to ancestor properties in style; rename view

--- LauncherToolTip/LauncherBehavior.cs
+++ LauncherToolTip/LauncherBehavior.cs
@@ -1,16 +1,20 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 
 namespace LauncherToolTip
 {
+    /// <summary>
+    /// Property binding for process launcher element
+    /// </summary>
     public static class LauncherBehavior
     {
+        #region properties
+
         public static readonly DependencyProperty PathProperty =
             DependencyProperty.RegisterAttached(
                 "Path", typeof(string),
                 typeof(LauncherBehavior),
-                new FrameworkPropertyMetadata()
+                new FrameworkPropertyMetadata
                 {
                     DefaultValue = "",
                     BindsTwoWayByDefault = false,
@@ -23,62 +27,13 @@ namespace LauncherToolTip
             return (string)fe.GetValue(PathProperty);
         }
 
-        public static void SetPath(FrameworkElement fe, string launcher)
+        public static void SetPath(FrameworkElement fe, string path)
         {
-            fe.SetValue(PathProperty, launcher);
-        }
-
-        public static readonly DependencyProperty ToolTipTextProperty =
-            DependencyProperty.RegisterAttached(
-                "ToolTipText", typeof(string),
-                typeof(LauncherBehavior), 
-                new FrameworkPropertyMetadata()
-                {
-                    DefaultValue = "",
-                    BindsTwoWayByDefault = false,
-                    PropertyChangedCallback = ToolTipTextPropertyChanged,
-                    DefaultUpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
-                });
-
-        public static string GetToolTipText(FrameworkElement fe)
-        {
-            return (string)fe.GetValue(ToolTipTextProperty);
-        }
-
-        public static void SetToolTipText(FrameworkElement fe, string launcher)
-        {
-            fe.SetValue(ToolTipTextProperty, launcher);
-        }
-
-        public static readonly DependencyProperty LauncherProperty =
-            DependencyProperty.RegisterAttached(
-                "Launcher", typeof(ProcessLauncher),
-                typeof(LauncherBehavior));
-
-        public static ProcessLauncher GetLauncher(FrameworkElement fe)
-        {
-            return (ProcessLauncher)fe.GetValue(LauncherProperty);
-        }
-
-        public static void SetLauncher(FrameworkElement fe, ProcessLauncher launcher)
-        {
-            fe.SetValue(LauncherProperty, launcher);
-        }
-
-        private static void ToolTipTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            DependencyObjectLoaded(d, e);
-            var obj = d as FrameworkElement;
-            var launcher = GetLauncher(obj);
-            if (launcher != null)
-            {
-                launcher.ToolTipText = e.NewValue as string;
-            }
+            fe.SetValue(PathProperty, path);
         }
 
         private static void PathPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            DependencyObjectLoaded(d, e);
             var obj = d as FrameworkElement;
             var launcher = GetLauncher(obj);
             if (launcher != null)
@@ -87,29 +42,54 @@ namespace LauncherToolTip
             }
         }
 
-        /// <summary>
-        /// onload handler
-        /// </summary>
-        /// <param name="depObj">attached element</param>
-        /// <param name="e">event arguments</param>
-        private static void DependencyObjectLoaded(DependencyObject depObj, DependencyPropertyChangedEventArgs e)
-        {
-            if (!(depObj is ContentControl element)) return;
-            if (GetLauncher(element) != null) return;
-
-            var launcher = new ProcessLauncher();
-            SetLauncher(element, launcher);
-
-            var oldContent = element.Content;
-            element.Content = new StackPanel
-            {
-                Orientation = Orientation.Horizontal,
-                Children =
+        public static readonly DependencyProperty ToolTipTextProperty =
+            DependencyProperty.RegisterAttached(
+                "ToolTipText", typeof(string),
+                typeof(LauncherBehavior), 
+                new FrameworkPropertyMetadata
                 {
-                    oldContent is UIElement el ? el : new TextBlock{ Text = (string)oldContent },
-                    launcher,
-                }
-            };
+                    DefaultValue = "",
+                    BindsTwoWayByDefault = false,
+                    PropertyChangedCallback = ToolTipTextPropertyChanged,
+                    DefaultUpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+                });
+
+
+        public static string GetToolTipText(FrameworkElement fe)
+        {
+            return (string)fe.GetValue(ToolTipTextProperty);
         }
+
+        public static void SetToolTipText(FrameworkElement fe, string text)
+        {
+            fe.SetValue(ToolTipTextProperty, text);
+        }
+
+        private static void ToolTipTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = d as FrameworkElement;
+            var launcher = GetLauncher(obj);
+            if (launcher != null)
+            {
+                launcher.ToolTipText = e.NewValue as string;
+            }
+        }
+
+        public static readonly DependencyProperty LauncherProperty =
+            DependencyProperty.RegisterAttached(
+                "Launcher", typeof(ProcessLauncherView),
+                typeof(LauncherBehavior));
+
+        public static ProcessLauncherView GetLauncher(FrameworkElement fe)
+        {
+            return (ProcessLauncherView)fe.GetValue(LauncherProperty);
+        }
+
+        public static void SetLauncher(FrameworkElement fe, ProcessLauncherView launcher)
+        {
+            fe.SetValue(LauncherProperty, launcher);
+        }
+
+        #endregion
     }
 }
--- LauncherToolTip/ProcessLauncherView.xaml.cs
+++ LauncherToolTip/ProcessLauncherView.xaml.cs
@@ -1,35 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LauncherToolTip
 {
     /// <summary>
-    /// Interaction logic for ProcessLauncher.xaml
+    /// Interaction logic for ProcessLauncherView.xaml
     /// </summary>
-    public partial class ProcessLauncher : UserControl
+    public partial class ProcessLauncherView : UserControl
     {
+        #region dependency properties
+
         public static readonly DependencyProperty PathProperty =
               DependencyProperty.Register(
                   "Path", typeof(string), 
-                  typeof(ProcessLauncher));
+                  typeof(ProcessLauncherView));
+
+        public string Path
+        {
+            get => (string)GetValue(PathProperty);
+            set => SetValue(PathProperty, value);
+        }
 
         public static readonly DependencyProperty ToolTipTextProperty =
               DependencyProperty.Register(
-                  "TooltipText", typeof(string),
-                  typeof(ProcessLauncher), new FrameworkPropertyMetadata()
+                  "ToolTipText", typeof(string),
+                  typeof(ProcessLauncherView), new FrameworkPropertyMetadata()
                   {
                       DefaultValue = "",
                       BindsTwoWayByDefault = true,
@@ -39,30 +36,26 @@ namespace LauncherToolTip
 
         private static void TooltipTextValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            var obj = d as ProcessLauncher;
+            var obj = (ProcessLauncherView)d;
             obj.TooltipLabel.Content = e.NewValue;
-        }
-
-        public string Path
-        {
-            get { return (string)GetValue(PathProperty); }
-            set { SetValue(PathProperty, value); }
         }
 
         public string ToolTipText
         {
-            get { return (string)GetValue(ToolTipTextProperty); }
-            set { SetValue(ToolTipTextProperty, value); }
+            get => (string)GetValue(ToolTipTextProperty);
+            set => SetValue(ToolTipTextProperty, value);
+        }
+
+        #endregion
+
+        public ProcessLauncherView()
+        {
+            InitializeComponent();
         }
 
         private void Hyperlink_Click(object sender, RoutedEventArgs e)
         {
             Process.Start(this.Path);
-        }
-
-        public ProcessLauncher()
-        {
-            InitializeComponent();
         }
     }
 }
